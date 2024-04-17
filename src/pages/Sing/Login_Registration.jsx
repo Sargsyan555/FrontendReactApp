096b1caedf482f826a37edc738944cb774d0959f
Sargsyan555 <sargsyanromik6@gmail.com>
2024-04-17 21:16:31 +0300

    changes

--- src/pages/Sing/Login_Registration.jsx
+++ src/pages/Sing/Login_Registration.jsx
@@ -11,31 +11,51 @@ export default function Login_Registration() {
     const [errors, setErrors] = useState({})
     const [valid, setValid] = useState(true)
     const navigate = useNavigate() 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+    const fetchUsers = () => {
+        return fetch('../../../db.json')
+            .then(response => response.json())
+            .catch(error => {
+                console.error('Error loading users:', error);
+                return [];
+            });
+    };
+    const userExists = ({users},  email) => {
+        return users.find(user => user.email === email);
+    };
+    
+    
     const handleSubmit = (event) => {
         
         event.preventDefault();
         let isValid = true
         let validationErrors = {}
-        // console.log(event.target.email.value);
 
-        if(event.target.email.value === "" || event.target.email.value == null){
+        if(event.target.email.value === "" || event.target.email.value == null || !emailRegex.test(event.target.email.value)){
             isValid = false
             validationErrors.email = "Emails is not valid"
         } 
-        if(event.target.password.value === "" || event.target.password.value == null){
+        if(event.target.password.value === "" || event.target.password.value == null || event.target.password.value.length < 6){
             isValid = false
             validationErrors.password = "Password is not valid"
         } 
         setErrors(validationErrors)
         setValid(isValid)
-        
-        if(Object.keys(validationErrors).length === 0){
-            axios.post('http://localhost:8000/users', formData)
-            .then(result => {
-                navigate('/userPage')
+        if(Object.keys(validationErrors).length === 0 && validationErrors){
+
+            fetchUsers().then(users => {
+                if (userExists(users, event.target.email.value)) {
+                    navigate('/userPage')
+                }else{
+                    axios.post('http://localhost:8000/users', formData)
+                    .then(result => {
+                        navigate('/userPage')
+                    })
+                    .catch(err => alert("error"))
+                }
             })
-            .catch(err => alert("error"))
+            
         }
     }
   
@@ -47,19 +67,24 @@ export default function Login_Registration() {
             <form className="form" action="" onSubmit={handleSubmit}>
                 {valid ? <></> : 
                 <span>
-                    {errors.email};{errors.pas}
+                    {errors.email}  
                 </span>
                 } 
                 <div className='insert'>
                     <label htmlFor="email">email</label>
                     <input onChange={(event)=>setFormData({...formData, email: event.target.value})} type="text" id='email'/>
                 </div>
+                {valid ? <></> : 
+                <span>
+                    {errors.password}
+                </span>
+                } 
                 <div className='insert'>
                     <label htmlFor="password">password</label>
                     <input onChange={(event)=>setFormData({...formData, password: event.target.value})} type="text" id='password'/>
                 </div>
                 <div className='buttons'>
-                    <button>cancl</button>
+                    <button onClick={() => handleClose()}>cancl</button>
                     <button>login</button>
                 </div>
             </form>
